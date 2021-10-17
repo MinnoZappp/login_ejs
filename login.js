86cf71b61f37e1b5fdb807ca62d8cb50ff3a2a43
Samitanun Sapsukdee <Samitanuns1@gmail.com>
2021-10-17 16:38:13 +0700

    First Login Nodejs

---
+++ login.js
@@ -0,0 +1,7 @@
+// 1.include the packages in our Node.js
+var mysql = require("mysql");
+var express = require("express");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var path = require("path");
+
